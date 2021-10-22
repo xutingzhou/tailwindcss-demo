b7c59263d1dc9c69da4506a9222b5af85c633681
xutingzhou <458970@gmail.com>
2021-10-22 12:34:15 +0800

    feat: config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {
+            backgroundColor: ['active', 'group-focus'],
+            divideColor: ['group-hover'],
+            scale: ['focus-within'],
+            animation: ['motion-reduce'],
+        },
+    },
+    plugins: [],
+}
